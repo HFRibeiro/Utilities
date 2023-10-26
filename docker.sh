6a3ac51418fa445ec18942f4eb7e2b01ec293f0a
Hélder Ribeiro <4473669+HFRibeiro@users.noreply.github.com>
2023-10-26 11:19:25 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -7,6 +7,5 @@ sudo apt-get update
 sudo apt install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -aG docker $USER
 newgrp docker
-docker version
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
